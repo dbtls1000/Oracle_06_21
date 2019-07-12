4402c5d91567b974b1bf330d553b1b06d502f5b3
dbtls1000 <dbsqhtjs@naver.com>
2019-07-12 16:27:52 +0900

    2019-07-12

--- user6-2019-07-11 books.sql
+++ user6-2019-07-11 books.sql
@@ -12,3 +12,14 @@ b_price number
 create SEQUENCE seq_book
 start with 1
 INCREMENT by 1;
+
+insert into tbl_books(b_seq, b_title,b_comp,b_author,b_price)
+values(seq_book.nextval,'모바일 웹','생능출판사','박성진',30000);
+
+insert into tbl_books(b_seq, b_title,b_comp,b_author,b_price)
+values(seq_book.nextval,'오라클SQL','길벗','홍형경',20000);
+
+insert into tbl_books(b_seq, b_title,b_comp,b_author,b_price)
+values(seq_book.nextval,'열혈자바','모름','모름',15000);
+
+commit;
